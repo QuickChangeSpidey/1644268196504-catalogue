8ae9f07885b517f2f68162edb7cb963a1b1d253a
Akshay Pandey <akshay.pandey@appliedsystems.com>
2022-07-08 03:20:21 -0400

    simple unit test

--- src/ui/__tests__/routes.spec.jsx
+++ src/ui/__tests__/routes.spec.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
-import {MemoryRouter} from 'react-router-dom';
-import {mount, configure} from 'enzyme';
-import App from '../components/App';
-import Home from '../components/Home';
-import NotFound from '../components/NotFound';
+import React from "react";
+import { MemoryRouter } from "react-router";
 
-describe('Application routes', () => {
-  it('should render a Home component', () => {
+import { mount } from "enzyme";
+import App from "../components/App";
+import Home from "../components/Home";
+import ButtonLink from "@tds/core-button-link";
+
+describe("Application routes", () => {
+  it("should render a Home component", () => {
     const wrapper = mount(
-      <MemoryRouter initialEntries={['/']}>
+      <MemoryRouter initialEntries={["/"]}>
         <App />
       </MemoryRouter>
     );
 
     expect(wrapper.find(Home)).toHaveLength(1);
+  });
+});
+
+describe("Render Component Test ", () => {
+  it("should render two ButtonLink components", () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={["/"]}>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(wrapper.find(ButtonLink)).toHaveLength(2);
   });
 });
