cbf6729e56e36283bbf56254ae2bf411b7a396f6
Akshay Pandey <akshay.pandey@appliedsystems.com>
2022-07-07 22:02:22 -0400

    iphone basic page

--- src/ui/components/Iphones/iPhoneCard.jsx
+++ src/ui/components/Iphones/iPhoneCard.jsx
@@ -1,27 +1,95 @@
 import React from "react";
 import Card from "@tds/core-card";
 import Box from "@tds/core-box";
-import ButtonLink from "@tds/core-button-link";
 import Heading from "@tds/core-heading";
 import Paragraph from "@tds/core-paragraph";
+import PriceLockup from "@tds/core-price-lockup";
+import { FootnoteLink } from "@tds/core-terms-and-conditions";
+import HairlineDivider from "@tds/core-hairline-divider";
+import FlexGrid from "@tds/core-flex-grid";
+import Image from "@tds/core-image";
 
-const IphoneCard = () => {
+const IphoneCard = (props) => {
   return (
-    <Card>
-      <Box between={4}>
+    <Box inline between={3}>
+      <Card>
         <Box between={3}>
-          <Heading level="h2">Need a hand?</Heading>
-          <Paragraph>
-            Ready to order? Have a question? We'll get back to you, with volume
-            discounts available to larger accounts.
-          </Paragraph>
+          <Image
+            src={"http://localhost:8081/images/iphone.jpg"}
+            alt="apple watch showing time"
+            width={200}
+            height={200}
+          />
         </Box>
-        <div>
-          {/* We wrap ButtonLink in a div to prevent flex styling from applying to it */}
-          <ButtonLink href="#">Request sales callback</ButtonLink>
-        </div>
-      </Box>
-    </Card>
+        <Box inset={1}>
+          <Heading level="h3">Apple</Heading>
+          <Heading level="h2">
+            Watch Series 6 (GPS + Cellular) - Aluminum
+          </Heading>
+
+          <Box vertical={2}>
+            <Paragraph size="medium">
+              Get a new smartphone for $0
+              <br />
+              on a 2-year plan.
+            </Paragraph>
+          </Box>
+
+          <FlexGrid gutter={false}>
+            <FlexGrid.Row horizontalAlign="start">
+              <FlexGrid.Col>
+                <Box>
+                  <PriceLockup
+                    size="small"
+                    topText="Starting at"
+                    price="25"
+                    signDirection="left"
+                    rateText="/month"
+                    bottomText="On a 2-year Easy Share Premium Plus Plan"
+                    a11yText="25 dollars per month"
+                    footnoteLinks={
+                      <FootnoteLink
+                        number={[7, 8]}
+                        onClick={(number, ref) => {}}
+                        copy="en"
+                      />
+                    }
+                  />
+                </Box>
+              </FlexGrid.Col>
+
+              <Box inset={2}>
+                <Box inline between={2}>
+                  <HairlineDivider vertical />
+                  <Box vertical={7} />
+                </Box>
+              </Box>
+
+              <FlexGrid.Col horizontalAlign={{ xs: "center", md: "left" }}>
+                <Box>
+                  <PriceLockup
+                    size="small"
+                    topText="Starting at"
+                    price="25"
+                    signDirection="left"
+                    rateText="/month"
+                    bottomText="On a 2-year Easy Share Premium Plus Plan"
+                    a11yText="25 dollars per month"
+                    footnoteLinks={
+                      <FootnoteLink
+                        number={[7, 8]}
+                        onClick={(number, ref) => {}}
+                        copy="en"
+                      />
+                    }
+                  />
+                </Box>
+              </FlexGrid.Col>
+            </FlexGrid.Row>
+          </FlexGrid>
+        </Box>
+      </Card>
+    </Box>
   );
 };
 
--- src/ui/components/Iphones/iPhonePage.jsx
+++ src/ui/components/Iphones/iPhonePage.jsx
@@ -3,20 +3,24 @@ import Heading from "@tds/core-heading";
 import Box from "@tds/core-box";
 import ButtonLink from "@tds/core-button-link";
 import Text from "@tds/core-text";
+import IphoneCard from "./iPhoneCard.jsx";
 
 const IphonePage = () => (
   <>
     <Box inset={4}>
       <Heading level="h1" tag="h2">
-        IPhones Demo
+        Iphone Demo
       </Heading>
       <Text>
         This is a Telus interview Iphone Demo built by Akshay Pandey on Jul 7,
         2022
       </Text>
     </Box>
-    <Box horizontal={4} vertical={3}>
+    <Box inset={4}>
       <ButtonLink href="/">Back</ButtonLink>
+    </Box>
+    <Box inset={4}>
+      <IphoneCard />
     </Box>
   </>
 );
